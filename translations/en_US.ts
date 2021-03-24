06d779de923cca029bbd866eb1d789f79082394c
cutefishd <cutefishos@foxmail.com>
2021-03-24 19:51:24 +0800

    Update translations

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -32,35 +32,4 @@
         <translation type="unfinished"></translation>
     </message>
 </context>
-<context>
-    <name>ControlDialog</name>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="178"/>
-        <source>Wi-Fi</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="181"/>
-        <location filename="../qml/ControlDialog.qml" line="204"/>
-        <source>On</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="181"/>
-        <location filename="../qml/ControlDialog.qml" line="193"/>
-        <location filename="../qml/ControlDialog.qml" line="204"/>
-        <source>Off</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="192"/>
-        <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="203"/>
-        <source>Dark Mode</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
 </TS>
--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -32,35 +32,4 @@
         <translation>应用启动器</translation>
     </message>
 </context>
-<context>
-    <name>ControlDialog</name>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="178"/>
-        <source>Wi-Fi</source>
-        <translation>无线网络</translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="181"/>
-        <location filename="../qml/ControlDialog.qml" line="204"/>
-        <source>On</source>
-        <translation>打开</translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="181"/>
-        <location filename="../qml/ControlDialog.qml" line="193"/>
-        <location filename="../qml/ControlDialog.qml" line="204"/>
-        <source>Off</source>
-        <translation>关闭</translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="192"/>
-        <source>Bluetooth</source>
-        <translation>蓝牙</translation>
-    </message>
-    <message>
-        <location filename="../qml/ControlDialog.qml" line="203"/>
-        <source>Dark Mode</source>
-        <translation>深色模式</translation>
-    </message>
-</context>
 </TS>
