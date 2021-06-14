0ea1273eeea69c45b7a5d2592fcf6aa1fece2e56
Weblate <noreply@weblate.org>
2021-06-14 16:51:20 +0200

    Added translation using Weblate (Esperanto)

---
+++ translations/eo_XX.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eo">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="73"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Unpin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Pin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="94"/>
+        <source>Close window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="95"/>
+        <source>Close %1 windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationModel</name>
+    <message>
+        <location filename="../src/applicationmodel.cpp" line="285"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="114"/>
+        <source>Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="124"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="129"/>
+        <source>Empty Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
