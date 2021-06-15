38fb82e0b179cbab30a71551863f32648b172ef3
Hosted Weblate <hosted@weblate.org>
2021-06-15 10:38:22 +0200

    Translated using Weblate (Nepali)
    Currently translated at 100.0% (9 of 9 strings)
    
    Co-authored-by: CutefishArch <8008698@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ne/
    Translation: CutefishOS/Dock

--- translations/ne_NP.ts
+++ translations/ne_NP.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../qml/AppItem.qml" line="73"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>खोल्ने</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="85"/>
         <source>Unpin</source>
-        <translation type="unfinished"></translation>
+        <translation>पिन नगर्ने</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="85"/>
         <source>Pin</source>
-        <translation type="unfinished"></translation>
+        <translation>पिन गर्ने</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="94"/>
         <source>Close window</source>
-        <translation type="unfinished"></translation>
+        <translation>दृश्य  बन्द गर्ने</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="95"/>
         <source>Close %1 windows</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 दृश्य बन्द गर्ने</translation>
     </message>
 </context>
 <context>
@@ -34,7 +34,7 @@
     <message>
         <location filename="../src/applicationmodel.cpp" line="285"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>लन्चर</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>रद्दी टोकरी</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="124"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>खोल्ने</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="129"/>
         <source>Empty Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>रद्दी खाली गर्ने</translation>
     </message>
 </context>
 </TS>
