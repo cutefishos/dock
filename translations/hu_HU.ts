b81585a88aa7ba45afb5f6e67f594174e64635dd
Hosted Weblate <hosted@weblate.org>
2021-06-25 13:09:27 +0200

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Added translation using Weblate (Hungarian)
    
    Co-authored-by: Benjamin Kőhidi <benstone326@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/hu/
    Translation: CutefishOS/Dock

---
+++ translations/hu_HU.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="73"/>
+        <source>Open</source>
+        <translation>Megnyitás</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Unpin</source>
+        <translation>Levétel</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Pin</source>
+        <translation>Kitűzés</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="94"/>
+        <source>Close window</source>
+        <translation>Ablak bezárása</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="95"/>
+        <source>Close %1 windows</source>
+        <translation>%1 ablak bezárása</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationModel</name>
+    <message>
+        <location filename="../src/applicationmodel.cpp" line="285"/>
+        <source>Launcher</source>
+        <translation>Indítópult</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="114"/>
+        <source>Trash</source>
+        <translation>Kuka</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="124"/>
+        <source>Open</source>
+        <translation>Megnyitás</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="129"/>
+        <source>Empty Trash</source>
+        <translation>Kuka ürítése</translation>
+    </message>
+</context>
+</TS>
