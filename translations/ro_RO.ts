fb2e6828b80686238d6102715caf6a83cd82c5a5
Hosted Weblate <hosted@weblate.org>
2021-08-17 23:53:09 +0200

    Translated using Weblate (Swahili)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translated using Weblate (Romanian)
    
    Currently translated at 88.8% (8 of 9 strings)
    
    Added translation using Weblate (Swahili)
    
    Added translation using Weblate (Romanian)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: circonfl3x <sayalelowesi456@gmail.com>
    Co-authored-by: ionut eugen <bueugenionut@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ro/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/sw/
    Translation: CutefishOS/Dock

---
+++ translations/ro_RO.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ro">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="73"/>
+        <source>Open</source>
+        <translation>Deschideți</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Unpin</source>
+        <translation>Anulați fixarea</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Pin</source>
+        <translation>Fixează</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="94"/>
+        <source>Close window</source>
+        <translation>Închideți fereastra</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="95"/>
+        <source>Close %1 windows</source>
+        <translation>Închideți ferestra %1</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationModel</name>
+    <message>
+        <location filename="../src/applicationmodel.cpp" line="285"/>
+        <source>Launcher</source>
+        <translation>Lansator</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="114"/>
+        <source>Trash</source>
+        <translation type="unfinished">Gunoi</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="124"/>
+        <source>Open</source>
+        <translation>Deschideți</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="129"/>
+        <source>Empty Trash</source>
+        <translation>Goliți coșul de gunoi</translation>
+    </message>
+</context>
+</TS>
