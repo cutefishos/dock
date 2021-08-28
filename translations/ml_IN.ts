449de9f91f462a673bddb99fef78c2e08d82aa36
Hosted Weblate <hosted@weblate.org>
2021-08-28 11:35:15 +0200

    Translated using Weblate (Russian)
    Currently translated at 88.8% (8 of 9 strings)
    
    Translated using Weblate (Romanian)
    
    Currently translated at 100.0% (9 of 9 strings)
    
    Translated using Weblate (Malayalam)
    
    Currently translated at 100.0% (9 of 9 strings)
    
    Co-authored-by: Alexey Ermakov <strangebrew@yandex.ru>
    Co-authored-by: Aswin A S <aswinas7469@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: ionut eugen <bueugenionut@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ml/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ro/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ru/
    Translation: CutefishOS/Dock

--- translations/ml_IN.ts
+++ translations/ml_IN.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../qml/AppItem.qml" line="73"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>തുറക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="85"/>
         <source>Unpin</source>
-        <translation type="unfinished"></translation>
+        <translation>അൺപിന്</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="85"/>
         <source>Pin</source>
-        <translation type="unfinished"></translation>
+        <translation>പിന് ചെയ്യുക</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="94"/>
         <source>Close window</source>
-        <translation type="unfinished"></translation>
+        <translation>ജാലകം അടക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="95"/>
         <source>Close %1 windows</source>
-        <translation type="unfinished"></translation>
+        <translation>1% ജാലകം അടക്കുക</translation>
     </message>
 </context>
 <context>
@@ -34,7 +34,7 @@
     <message>
         <location filename="../src/applicationmodel.cpp" line="285"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>ലോഞ്ച്ർ</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>ചവറ്റുകുട്ട</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="124"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>തുറക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="129"/>
         <source>Empty Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>ചവറ്റുകുട്ട ശൂന്യമാക്കുക</translation>
     </message>
 </context>
 </TS>
