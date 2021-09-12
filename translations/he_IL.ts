f8d9c94a1df532ad69b3966dd900bc0f8aa6128a
Hosted Weblate <hosted@weblate.org>
2021-09-12 15:34:18 +0200

    Translated using Weblate (Latvian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Added translation using Weblate (Latvian)
    
    Translated using Weblate (Hebrew (Israel))
    
    Currently translated at 77.7% (7 of 9 strings)
    
    Added translation using Weblate (Hebrew (Israel))
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: bugthug <avivyodfat@protonmail.com>
    Co-authored-by: ツ GUCKII ツ <mariusskrasavcevs@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/he_IL/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/lv/
    Translation: CutefishOS/Dock

---
+++ translations/he_IL.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he_IL">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="73"/>
+        <source>Open</source>
+        <translation>פתח</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Unpin</source>
+        <translation>בטל הצמדה</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="85"/>
+        <source>Pin</source>
+        <translation>הצמד</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="94"/>
+        <source>Close window</source>
+        <translation type="unfinished">סגור חלון</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="95"/>
+        <source>Close %1 windows</source>
+        <translation>סגור %1 חלונות</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationModel</name>
+    <message>
+        <location filename="../src/applicationmodel.cpp" line="285"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="114"/>
+        <source>Trash</source>
+        <translation>סל המיחזור</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="124"/>
+        <source>Open</source>
+        <translation>פתח</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="129"/>
+        <source>Empty Trash</source>
+        <translation>רוקן את סל המיחזור</translation>
+    </message>
+</context>
+</TS>
