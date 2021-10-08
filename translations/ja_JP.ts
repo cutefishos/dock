1194e01512649f8ff569e690fa46d39785c875e4
Hosted Weblate <hosted@weblate.org>
2021-10-08 04:30:08 +0200

    Translated using Weblate (Japanese)
    Currently translated at 55.5% (5 of 9 strings)
    
    Translated using Weblate (Russian)
    
    Currently translated at 100.0% (9 of 9 strings)
    
    Added translation using Weblate (Japanese)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: TaIenT <1054001563@qq.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Фёдор <fedor010908@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ja/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/dock/ru/
    Translation: CutefishOS/Dock

--- translations/ja_JP.ts
+++ translations/ja_JP.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru">
+<TS version="2.1" language="ja">
 <context>
     <name>AppItem</name>
     <message>
         <location filename="../qml/AppItem.qml" line="73"/>
         <source>Open</source>
-        <translation>Открыть</translation>
+        <translation>開く</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="85"/>
         <source>Unpin</source>
-        <translation>Открепить</translation>
+        <translation type="unfinished">解除</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="85"/>
         <source>Pin</source>
-        <translation>Закрепить</translation>
+        <translation type="unfinished">固定</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="94"/>
         <source>Close window</source>
-        <translation>Закрыть окно</translation>
+        <translation type="unfinished">ウィンドウを閉じる</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="95"/>
         <source>Close %1 windows</source>
-        <translation>Закрыть %1 окна</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -34,7 +34,7 @@
     <message>
         <location filename="../src/applicationmodel.cpp" line="285"/>
         <source>Launcher</source>
-        <translation>Лаунчер</translation>
+        <translation>ランチャー</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Trash</source>
-        <translation>Корзина</translation>
+        <translation>ゴミ箱</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="124"/>
         <source>Open</source>
-        <translation>Открыть</translation>
+        <translation>開く</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="129"/>
         <source>Empty Trash</source>
-        <translation>Очистить корзину</translation>
+        <translation>ゴミ箱を空にする</translation>
     </message>
 </context>
 </TS>
